8b822813eb0325ae34e43966da3f1a90b15d33c3
ferueda <fe.rueda@outlook.com>
2022-02-09 19:07:03 -0800

    Create user sql

---
+++ internal/user/sql.go
@@ -0,0 +1,24 @@
+package user
+
+// CREATE TABLE IF NOT EXISTS users (
+// 	id CHAR(27) NOT NULL UNIQUE,
+// 	email VARCHAR(255) NOT NULL,
+// 	username VARCHAR(255) NOT NULL,
+// 	created_at TIMESTAMP,
+// 	PRIMARY KEY (id)
+// );
+// ALTER TABLE users DROP COLUMN username;
+
+// CREATE TABLE IF NOT EXISTS user_sign_on_token (
+// 	token CHAR(27) NOT NULL UNIQUE,
+// 	email VARCHAR(255) NOT NULL
+// );
+
+// CREATE INDEX user_sign_on_token_token_idx on user_sign_on_token (token);
+
+// CREATE TABLE IF NOT EXISTS edit_token (
+//   token      CHAR(27) NOT NULL,
+//   job_id     INTEGER NOT NULL REFERENCES job (id),
+//   created_at TIMESTAMP NOT NULL
+// );
+// CREATE UNIQUE INDEX token_idx on edit_token (token);
