1172d01639148f8aa82f85fa876b0b155c0438e1
GolangCafe <team@golang.cafe>
2022-02-23 00:43:32 +0000

    more updates

--- run-local-webserver.sh
+++ run-local-webserver.sh
@@ -31,6 +31,7 @@ export TELEGRAM_CHANNEL_ID=1233123123
 export FX_API_KEY=a123
 export SITE_NAME="Golang Cafe"
 export SITE_JOB_CATEGORY="golang"
+export SITE_HOST="golang.cafe"
 
 go build -o bin/server cmd/server/main.go
 
