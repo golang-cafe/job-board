0c43c74f0e78a67d8e8fc3f50837f77bb88357b0
dm161 <x@diego.pizza>
2022-05-01 21:56:14 +0100

    tweaks

--- latest-schema.sql
+++ latest-schema.sql
@@ -769,3 +769,18 @@ ALTER TABLE ONLY public.purchase_event
 -- PostgreSQL database dump complete
 --
 
+CREATE TABLE IF NOT EXISTS public.blog_post (
+	id CHAR(27) NOT NULL,
+	title VARCHAR(255) NOT NULL,
+	description VARCHAR(255) NOT NULL,
+	slug VARCHAR(255) NOT NULL,
+	tags VARCHAR(255) NOT NULL,
+	text TEXT NOT NULL,
+	created_at TIMESTAMP NOT NULL,
+	updated_at TIMESTAMP NOT NULL,
+	created_by CHAR(27) NOT NULL,
+	published_at TIMESTAMP DEFAULT NULL,
+	PRIMARY KEY (id)
+);
+
+ CREATE UNIQUE INDEX blog_post_slug_idx on public.blog_post (slug);
