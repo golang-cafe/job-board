db85a60310cfac9c30d96b477b89eef4294c0396
dm161 <x@diego.pizza>
2023-04-25 19:54:33 +0100

    minor

--- internal/handler/handlers.go
+++ internal/handler/handlers.go
@@ -3462,6 +3462,7 @@ func EditJobViewPageHandler(svr server.Server, jobRepo *job.Repository) http.Han
 		}
 		svr.Render(r, w, http.StatusOK, "edit.html", map[string]interface{}{
 			"Job":                        jobPost,
+			"HowToApplyIsURL":		      svr.IsEmail(jobPost.HowToApply),
 			"Stats":                      string(statsSet),
 			"Purchases":                  purchaseEvents,
 			"JobPerksEscaped":            svr.JSEscapeString(jobPost.Perks),
